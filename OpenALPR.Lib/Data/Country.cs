22aba4b36575fdb0709e08557369c44887d571ad
Grant Hair <grant.hair@arnoldclark.com>
2018-10-26 09:47:20 +0100

    initial commit

---
+++ OpenALPR.Lib/Data/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace OpenALPR.Lib.Data
+{
+    public enum Country
+    {
+        US,
+        EU,
+        AU,
+        GB,
+        KR,
+        SG
+    }
+}
---
+++ OpenALPR.Lib/Data/Result.cs
@@ -0,0 +1,8 @@
+﻿namespace OpenALPR.Lib.Data
+{
+    public class Result
+    {
+        public string PlateNumber { get; set; }
+        public float Confidence { get; set; }
+    }
+}
